59403e45503a839e4b27a61d65c56138e722c01b
RFID Lab <calebnbrown2004@gmail.com>
2024-05-13 16:25:39 -0500

    Changed the manager around a bit.

--- RFIDAppNew/RFIDManager.swift
+++ RFIDAppNew/RFIDManager.swift
@@ -10,18 +10,44 @@ import Combine
 
 class RFIDViewModel: ObservableObject {
     @Published var readers: [String] = []
-    var apiInstance: srfidISdkApi = srfidSdkFactory.createRfidSdkApiInstance()
+    let _api: srfidISdkApi = srfidSdkFactory.createRfidSdkApiInstance()
+    let apiDelegate: SdkApiDelegate
+    
+    init() {
+        self.apiDelegate = SdkApiDelegate(api: _api)
+        
+        _api.srfidSetOperationalMode(Int32(SRFID_OPMODE_ALL))
+        _api.srfidSubsribe(forEvents: Int32(SRFID_EVENT_READER_APPEARANCE) | Int32(SRFID_EVENT_READER_DISAPPEARANCE) | Int32(SRFID_EVENT_SESSION_ESTABLISHMENT) | Int32(SRFID_EVENT_SESSION_TERMINATION))
+        _api.srfidSubsribe(forEvents: Int32(SRFID_EVENT_MASK_READ) | Int32(SRFID_EVENT_MASK_STATUS) | Int32(SRFID_EVENT_MASK_STATUS_OPERENDSUMMARY))
+        _api.srfidSubsribe(forEvents: Int32(SRFID_EVENT_MASK_TEMPERATURE) | Int32(SRFID_EVENT_MASK_POWER) | Int32(SRFID_EVENT_MASK_DATABASE))
+        _api.srfidSubsribe(forEvents: Int32(SRFID_EVENT_MASK_PROXIMITY) | Int32(SRFID_EVENT_MASK_TRIGGER) | Int32(SRFID_EVENT_MASK_BATTERY))
+        _api.srfidEnableAvailableReadersDetection(true)
+        _api.srfidEnableAutomaticSessionReestablishment(true)
+        _api.srfidSetDelegate(self.apiDelegate)
+    }
 
     func getAvailableReaderList() {
-        var availableReaders: NSMutableArray? = NSMutableArray()
-        apiInstance.srfidEnableAvailableReadersDetection(true)
-        apiInstance.srfidSetOperationalMode(Int32(SRFID_OPMODE_MFI))
-        apiInstance.srfidGetAvailableReadersList(&availableReaders)
-        print(availableReaders)
-        
-        if let readers = availableReaders as? [srfidReaderInfo] {
-            self.readers = readers.map { $0.getReaderName() }
+        let devices: NSMutableArray = NSMutableArray()
+        var _devices = devices
+        apiDelegate.availableReaders = withUnsafeMutablePointer(to: &_devices) { ptr -> Array<srfidReaderInfo>? in
+            let devicesPtr : AutoreleasingUnsafeMutablePointer<NSMutableArray?> = AutoreleasingUnsafeMutablePointer<NSMutableArray?>.init(ptr)
+            let r = _api.srfidGetAvailableReadersList(devicesPtr)
+            if r == SRFID_RESULT_SUCCESS {
+                return (devices.map({ obj in
+                    obj as! srfidReaderInfo
+                }))
+            }
+            else {
+                print("Unable to get available readers")
+            }
+            return nil
         }
+        print(apiDelegate.availableReaders!.map({ (reader: srfidReaderInfo) -> [String : Any] in
+            return [
+                "name" : reader.getReaderName()!,
+                "id" : "\(reader.getReaderID())"
+            ]
+        }))
     }
 }
 
