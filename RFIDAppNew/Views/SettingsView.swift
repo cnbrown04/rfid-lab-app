e0affb3b0c7bdab57905beb95931e6878bd6aa5f
RFID Lab <calebnbrown2004@gmail.com>
2024-05-16 14:49:52 -0500

    Add version to settings.

--- RFIDAppNew/Views/SettingsView.swift
+++ RFIDAppNew/Views/SettingsView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct SettingsView: View {
     @State private var scannerIndex = 0
+    @State var label = "Connect"
     @ObservedObject var viewModel = RFIDViewModel()
+    @State private var connectAlert = false
+    @State private var failAlert = false
+    @State private var alrConnectedAlert = false
+    let versionString: String
+    let appIcon: String
     
     var body: some View {
         let readers = viewModel.getAvailableReaderList()
@@ -20,7 +26,7 @@ struct SettingsView: View {
                     HStack {
                         Image(systemName: "qrcode.viewfinder")
                         Picker(selection: $scannerIndex, label: Text("Scanner")) {
-                            Text("Pick a scanner").tag(0)
+                            Text("Pick A Scanner").tag(0)
                             ForEach(readers) { reader in
                                 Text(reader.name).tag(Int(reader.id))
                             }
@@ -29,15 +35,56 @@ struct SettingsView: View {
                     }
                     if viewModel.isActiveReader() {
                         Button("Connected") {
-                            // Handle connected action
+                            alrConnectedAlert = true
                         }
                         .frame(maxWidth: .infinity, alignment: .center).padding()
+                        .foregroundColor(.green)
+                        .alert("Already connected!", isPresented: $alrConnectedAlert) {
+                            Button("OK") { }
+                        }
                     } else {
-                        Button("Connect") {
+                        Button(label) {
                             let result = viewModel.connectToReader(reader: scannerIndex)
+                            if (result) {
+                                label = "Connected"
+                                connectAlert = true
+                                scannerIndex = 0
+                            } else {
+                                label = "Failed"
+                                failAlert = true
+                            }
                         }
                         .frame(maxWidth: .infinity, alignment: .center).padding()
+                        .alert("Connected!", isPresented: $connectAlert) {
+                            Button("OK") {}
+                        }
+                        .alert("Failure!", isPresented: $failAlert) {
+                            Button("OK") {}
+                        }
                     }
+                })
+                Section(header: Text("APP INFO"), content: {
+                    HStack(alignment: .center, spacing: 12) {
+                        // App icons can only be retrieved as named `UIImage`s
+                        // https://stackoverflow.com/a/62064533/17421764
+                        if let image = UIImage(named: appIcon) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                        }
+                        VStack(alignment: .leading) {
+                            Text("Version")
+                                .fontWeight(.semibold)
+                                .font(.headline)
+                            Text("v\(versionString)")
+                                .fontWeight(.regular)
+                                .font(.subheadline)
+                        }
+                        .foregroundColor(.primary)
+                    }
+                    .fixedSize()
+                    .frame(maxWidth: .infinity, alignment: .center)
                 })
             }
         }
