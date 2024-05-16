4719ff1af8d92a3c385921f8f9b1baa6678fe1c0
RFID Lab <calebnbrown2004@gmail.com>
2024-05-16 14:49:14 -0500

    Add a verison provider.

--- RFIDAppNew/Providers/AppVersionProvider.swift
+++ RFIDAppNew/Providers/AppVersionProvider.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+enum AppVersionProvider {
+    static func appVersion(in bundle: Bundle = .main) -> String {
+        guard let version = bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+            fatalError("CFBundleShortVersionString should not be missing from info dictionary")
+        }
+        return version
+    }
+}
