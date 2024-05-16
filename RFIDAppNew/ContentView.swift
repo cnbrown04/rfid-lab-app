c58025868191dbdb21c027fa0020abfd24d1789b
RFID Lab <calebnbrown2004@gmail.com>
2024-05-16 14:49:05 -0500

    Add Navigation Bar.

--- RFIDAppNew/ContentView.swift
+++ RFIDAppNew/ContentView.swift
@@ -8,58 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var defaultSel = 3
    
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color(UIColor.systemGroupedBackground)
-                    .ignoresSafeArea()
-                VStack {
-                    HStack {
-                        NavigationLink(destination: AuditView(), label: {
-                            VStack {
-                                Label("Audit", systemImage: "list.clipboard.fill")
-                                Button(action: {}) {
-                                    
-                                }
-                                .frame(width: 100)
-                            }
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(.blue)
-                            .cornerRadius(8)
-                        })
-                        NavigationLink(destination: TestingView(), label: {
-                            VStack {
-                                Label("Testing", systemImage: "testtube.2")
-                                Button(action: {}) {
-                                    
-                                }
-                                .frame(width: 100)
-                            }
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(.orange)
-                            .cornerRadius(8)
-                        })
-                    }
-                    HStack {
-                        NavigationLink(destination: SettingsView(), label: {
-                            VStack {
-                                Label("Settings", systemImage: "gearshape.fill")
-                                Button(action: {}) {
-                                    
-                                }
-                                .frame(width: 240)
-                            }
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(.gray)
-                            .cornerRadius(8)
-                        })
-                    }
+        TabView(selection: $defaultSel) {
+            AuditView()
+                .tabItem {
+                    Label("Audit", systemImage: "list.bullet.clipboard.fill")
                 }
-            }
+                .tag(1)
+            InventoryView()
+                .tabItem {
+                    Label("Inventory", systemImage: "tray.2.fill")
+                }.tag(2)
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }.tag(3)
+            TestingView()
+                .tabItem {
+                    Label("Testing", systemImage: "testtube.2")
+                }.tag(4)
+            SettingsView(versionString: AppVersionProvider.appVersion(), appIcon: AppIconProvider.appIcon())
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape.fill")
+                }.tag(5)
         }
     }
 }
